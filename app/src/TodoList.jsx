2bc646e0dff21df049ccc84f8f3709638f759f3b
Disaster-Loki <sebastiaol.carvalho22@gmail.com>
2024-06-07 09:47:04 -0400

    Version 1.3

--- app/src/TodoList.jsx
+++ app/src/TodoList.jsx
@@ -1,28 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 import './TodoList.css';
 
 function TodoList()
 {
+    const [list, setList] = useState([]);
+    const [newItem, setNewItem] = useState("");
+
     return (
         <div>
             <h2>To Do List</h2>
             <form>
-                <input type="text" 
-                placeholder="Add a Task"/>
+                <input type="text" placeholder="Add a Task"/>
                 <button className="add" type="submit">Add</button>
             </form>
             <div className="listTask">
                 <div className="item">
                     <span>Example Task</span>
-                    <button>Delete</button>
+                    <button className="del">Delete</button>
                 </div>
                 <div className="item complet">
                     <span>Example Task</span>
-                    <button>Delete</button>
+                    <button className="del">Delete</button>
                 </div>
+                <button className="delAll">All Delete</button>
             </div>
         </div>
     )
 }
-
+ 
 export default TodoList;
