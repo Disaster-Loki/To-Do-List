48b0956aa42127fe2c03589b05f1a6c9307b8241
Disaster-Loki <sebastiaol.carvalho22@gmail.com>
2024-06-07 10:30:56 -0400

    Version 1.4

--- app/src/TodoList.jsx
+++ app/src/TodoList.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import Icone from './assets/icon.png'
 import './TodoList.css';
 
 function TodoList()
@@ -6,21 +7,50 @@ function TodoList()
     const [list, setList] = useState([]);
     const [newItem, setNewItem] = useState("");
 
+    function addItem(form){
+        form.preventDefault();
+        if (!newItem)
+            return;
+        setList([...list, {text: newItem, isCompleted: false}]);
+        setNewItem("");
+        document.getElementById("enter").focus();
+    }
+
+    function click(index){
+        const listAux = [...list];
+        listAux[index].isCompleted = !listAux[index].isCompleted;
+        setList(listAux);
+    }
+
+    function del(index){
+        const listAux = [...list]
+    }
+
     return (
         <div>
             <h2>To Do List</h2>
-            <form>
-                <input type="text" placeholder="Add a Task"/>
+            <form onSubmit={addItem}>
+                <input id="enter" type="text" value={newItem}
+                onChange={(e)=> {setNewItem(e.target.value)}} 
+                placeholder="Add a Task"/>
                 <button className="add" type="submit">Add</button>
             </form>
             <div className="listTask">
-                <div className="item">
-                    <span>Example Task</span>
-                    <button className="del">Delete</button>
-                </div>
-                <div className="item complet">
-                    <span>Example Task</span>
-                    <button className="del">Delete</button>
+                <div>
+                    {
+                         list.length < 1
+                        ?
+                        <img className="icon-center" src={Icone} />
+                        :
+                          list.map((item, index) => (
+                            <div key={index}
+                            className={item.isCompleted ? "item complet" : "item"}>
+                              <span onClick={()=>click(index)} >{item.text}</span>
+                              <button  onClick={()=>del(index)} 
+                              className="del">Delete</button>
+                            </div>
+                          ))
+                      }
                 </div>
                 <button className="delAll">All Delete</button>
             </div>
